81c6d84692d54df78bf79494cb720bd09207699d
Alejandro Hernandez <alejandro@brainhub.pl>
2018-06-24 23:27:33 +0200

    Chore: Added ts boilerplatte and deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,38 @@
 module.exports = {
-    parser: 'babel-eslint',
-    extends: 'airbnb-base',
-    env: {
-      node: true,
+  parser: 'typescript-eslint-parser',
+  extends: 'airbnb-base',
+  env: {
+    node: true,
+  },
+  plugins: [
+    'typescript',
+  ],
+  rules: {
+    'import/extensions': ['error', 'ignorePackages', {
+      js: 'never',
+      mjs: 'never',
+      jsx: 'never',
+      ts: 'never',
+      '.ts': 'never',
+    }],
+    'max-len': ['error', { code: 120 }],
+  },
+  settings: {
+    'import/resolver': {
+      'eslint-import-resolver-typescript': true,
     },
-  };
-  
+  },
+  overrides: {
+    files: ['**/*.ts'],
+    parser: 'typescript-eslint-parser',
+    plugins: [
+      'typescript',
+    ],
+    rules: {
+      'no-undef': 'off',
+      'typescript/class-name-casing': 2,
+      'no-unused-vars': 0,
+      'typescript/no-unused-variable': true,
+    },
+  },
+};
